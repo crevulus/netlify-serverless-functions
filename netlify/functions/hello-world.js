900c42da61880890641e65412c402a6958b359a9
crevulus <crevulus@gmail.com>
2022-11-11 14:30:36 +0000

    dummy console log for hello world fn

--- netlify/functions/hello-world.js
+++ netlify/functions/hello-world.js
@@ -1,8 +1,9 @@
 export const handler = async () => {
+  console.log("this is a console log");
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Hello world!'
-    })
-  }
-}
+      message: "Hello world!",
+    }),
+  };
+};
